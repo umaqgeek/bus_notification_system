5e86299dd2bc1f2bc3bd3be74fec00462b3f578f
amarule <amarulsamsudin@gmail.com>
2015-08-25 14:34:56 +0000

    Log In Page

--- application/views/hello.php
+++ application/views/hello.php
@@ -1,5 +1,15 @@
-<html>
-<body>
-<h>Hello World</h>
-</body>
+<html >
+  <head>
+  </head>
+  <body>
+	<div class="container">
+		<h1>WelcomeTo Login Page</h1>
+		
+		<form class="form">
+			<input type="text" placeholder="Username">
+			<input type="password" placeholder="Password">
+			<button type="submit" id="login-button">Login</button>
+		</form>
+	</div>
+  </body>
 </html>
