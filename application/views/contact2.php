bea09b79797dd0f4f230d28277aa225259fb8215
amarule <amarulsamsudin@gmail.com>
2015-09-02 14:25:48 +0000

    Update Driver Page

--- application/views/contact2.php
+++ application/views/contact2.php
@@ -6,18 +6,18 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Contact Us 2  | Triangle</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/prettyPhoto.css" rel="stylesheet"> 
-    <link href="css/animate.min.css" rel="stylesheet"> 
-	<link href="css/main.css" rel="stylesheet">
-	<link href="css/responsive.css" rel="stylesheet">
+    <link href="<?php echo base_url('asset/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('asset/css/font-awesome.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('asset/css/prettyPhoto.css'); ?>" rel="stylesheet"> 
+    <link href="<?php echo base_url('asset/css/animate.min.css'); ?>" rel="stylesheet"> 
+	<link href="<?php echo base_url('asset/css/main.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('asset/css/responsive.css'); ?>" rel="stylesheet">
 
     <!--[if lt IE 9]>
 	    <script src="js/html5shiv.js"></script>
 	    <script src="js/respond.min.js"></script>
     <![endif]-->       
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="shortcut icon" href="<?php echo base_url('asset/images/ico/favicon.ico'); ?>">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
@@ -52,7 +52,7 @@
                     </button>
 
                     <a class="navbar-brand" href="index.html">
-                        <h1><img src="images/logo.png" alt="logo"></h1>
+                        <h1><img src="<?php echo base_url('asset/images/logo.png'); ?>" alt="logo"></h1>
                     </a>
                     
                 </div>
@@ -193,11 +193,11 @@
     <!--/#footer-->
 
 
-    <script type="text/javascript" src="js/jquery.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
-    <script type="text/javascript" src="js/gmaps.js"></script>
-    <script type="text/javascript" src="js/wow.min.js"></script>
-    <script type="text/javascript" src="js/main.js"></script>   
+    <script type="text/javascript" src="<?php echo base_url('asset/js/jquery.js'); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('asset/js/bootstrap.min.js'); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('asset/http://maps.google.com/maps/api/js?sensor=true'); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('asset/js/gmaps.js'); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('asset/js/wow.min.js'); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('asset/js/main.js'); ?>"></script>   
 </body>
 </html>
