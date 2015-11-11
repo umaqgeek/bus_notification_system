4c8c969f83554aa7fab3468f5d5fcdfbef114478
amarule <amarulsamsudin@gmail.com>
2015-11-11 14:17:03 +0000

    External task - Email Template DinarPal

---
+++ External Task/Amarul/email template/template 2.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<style>
+
+div.bg_yellow {
+  background-image: url("email.jpg");
+  position: relative;
+  left: 0px;
+  top: 0px;
+  width: 676px;
+  height: 578px;
+  z-index: 1;
+}
+
+h2 span { 
+   color: white; 
+   font: bold 24px/45px Helvetica, Sans-Serif; 
+   letter-spacing: -1px;  
+   background: rgb(0, 0, 0); 
+   background: rgba(0, 0, 0, 0.0);
+   position: relative;
+   padding: 40px; 
+   left: 150px;
+   top: 10;
+}
+
+p { 
+   color: black; 
+   font: Sans-Serif;   
+   position: relative; 
+   left: 25px;
+   top: 10;
+   right: 800px;
+   text-align: justify;
+}
+
+
+</style>
+</head>
+<body>
+
+<div class="bg_yellow">
+<img src="staff.png" style="position: relative; top: 90; left:180;" />
+<img src="blue_btn.png" style="position: relative; top: 80; left:0;" />
+<h2><span>Successfully Registered</span></h2>
+<p>Your account have successfully verified and created in DinarPal account. <br />
+Your username is shown below. You will need this along with the password you choose <br />
+everytime you login.</p>
+
+<p><b>Username :</b> pisangbyte</p>
+
+<p>Click the Login button below then enter your Username and password in the DinarPal login page.</p>
+<img src="login.png" style="position: relative; top: 0; left:20;" width="80"/>
+</div>
+</body>
+</html>
+
---
+++ External Task/Amarul/email template/template.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+<style>
+
+div.bg_yellow {
+  background-image: url("email.jpg");
+  position: relative;
+  left: 0px;
+  top: 0px;
+  width: 676px;
+  height: 578px;
+  z-index: 1;
+}
+
+h2 span { 
+   color: white; 
+   font: bold 24px/45px Helvetica, Sans-Serif; 
+   letter-spacing: -1px;  
+   background: rgb(0, 0, 0); 
+   background: rgba(0, 0, 0, 0.0);
+   position: relative;
+   padding: 40px; 
+   left: 180px;
+   top: 10;
+}
+
+p { 
+   color: black; 
+   font: Sans-Serif;   
+   position: relative; 
+   left: 25px;
+   top: 10;
+   right: 800px;
+   text-align: justify;
+}
+
+
+</style>
+</head>
+<body>
+
+<div class="bg_yellow">
+<img src="staff.png" style="position: relative; top: 90; left:180;" />
+<img src="blue_btn.png" style="position: relative; top: 80; left:0;" />
+<h2><span>Account Verification</span></h2>
+<p>Thank you for registering to DinarPal. To complete your registration you need to verify your <br />account by using
+the link given.</p>
+
+<p>Link :<a href="http://www.dinarpal.com/user/verify/?key=BHsmdsa12Bhyae74Ty" target="_blank">http://www.dinarpal.com/user/verify/?key=BHsmdsa12Bhyae74Ty</a>
+
+<br /><br />This will help protect you identity and allows you to use the full range of our services in DinarPal. 
+<br />Thank you.
+
+</p>
+</div>
+</body>
+</html>
+
