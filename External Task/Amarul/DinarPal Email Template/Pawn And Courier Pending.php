ef4b7f844f1fc64845ce5872222cbbbff492a585
amarule <amarulsamsudin@gmail.com>
2015-12-16 06:05:50 +0000

    External task - Email Template DinarPal(Finish)

---
+++ External Task/Amarul/DinarPal Email Template/Pawn And Courier Pending.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<style>
+
+div.bg_yellow {
+  background-image: url("email.jpg");
+  position: relative;
+  left: 0px;
+  top: 0px;
+  width: 676px;
+  height: 578px;
+  z-index: 1;
+}
+
+h2 span { 
+   color: white; 
+   font: bold 24px/45px Helvetica, Sans-Serif; 
+   letter-spacing: -1px;  
+   background: rgb(0, 0, 0); 
+   background: rgba(0, 0, 0, 0.0);
+   position: relative;
+   padding: 40px; 
+   left: 130px;
+   top: -10;
+}
+
+p { 
+   color: black; 
+   font: Sans-Serif;   
+   position: relative; 
+   left: 25px;
+   top: -20;
+   right: 800px;
+   text-align: justify;
+}
+</style>
+</head>
+<body>
+
+<div class="bg_yellow">
+<img src="pawn pending.png" style="position: relative; top: 90; left:240;" height="150" width="150"/>
+<img src="blue_btn.png" style="position: relative; top: 60; left:0;" />
+<h2><span>Pawn & Courier Pending</span></h2>
+<p>Dear pisangbyte, <br /><br />
+You Pawn and Courier is Pending. Please wait until our system successfully receive it.<br />
+Please check the transaction status in your <a href="">Account Detail</a>.<br />
+<br />
+<b>Username :</b> pisangbyte <br />
+<b>Total Weight (gram) :</b> 4.25<br />
+<b>Grand Total (MYR) :</b> 558.58 <br />
+<b>Collateral Rate (%) :</b>80.00<br /><br />
+
+If you have any enquiries, please contact our <a href="">Customer Support.</a></p>
+</div>
+</body>
+</html>
+
